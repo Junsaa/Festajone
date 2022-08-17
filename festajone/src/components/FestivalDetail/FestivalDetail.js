00e1ef327ab9653d59467413c0a5ee3975efeb6b
노소연 <yeonie@kakao.com>
2022-08-17 17:48:40 +0900

    css 파일 정리, 컴포넌트 정리

--- festajone/src/components/FestivalDetail/FestivalDetail.js
+++ festajone/src/components/FestivalDetail/FestivalDetail.js
@@ -1,14 +1,13 @@
 /* global kakao */
-//npm i bootstrap-icons
 import React from 'react';
 import Map from '../MapApi/Map';
 import './festivalDetail.css';
-import { Card, Carousel } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 
 const FestivalDetail = () => {
   return (
     <>
-      <table width="100%" height="100px" align="center" style={{ padding: '30px' }}>
+      <table className="table">
         <tr>
           <td style={{ textAlign: 'center' }}>
             <h2>축제 이름</h2>
@@ -17,39 +16,37 @@ const FestivalDetail = () => {
           <td>
             <h1>
               <i class="bi bi-heart"></i>
+              {/* 채워진 하트 <i class="bi bi-heart-fill"></i> */}
             </h1>
           </td>
         </tr>
       </table>
 
-      {/* 이미지가 이미지마다 크기가 다르면? */}
+      {/* 이미지 슬라이드 */}
       <DarkVariantExample></DarkVariantExample>
 
-      <div width="100%" height="400px" style={{ padding: '20px' }}>
+      <div className="detail_div">
         <strong>상세정보</strong>
         <hr style={{ height: '1px', background: 'black' }} />
-        {/* <Card>
-          <Card.Body> */}
+
         <ul>
           <li>
-            <b className="b">시작일</b>
-            <span className="span">2022.09.01</span>
+            <b className="detail_b">시작일</b>
+            <span className="detail_span">2022.09.01</span>
           </li>
           <li>
-            <b className="b">종료일</b>
-            <span className="span">2022.09.01</span>
+            <b className="detail_b">종료일</b>
+            <span className="detail_span">2022.09.01</span>
           </li>
           <li>
-            <b className="b">전화번호</b>
-            <span className="span"> 043-830-3463</span>
+            <b className="detail_b">전화번호</b>
+            <span className="detail_span"> 043-830-3463</span>
           </li>
           <li>
-            <b className="b">주소</b>
-            <span className="span"> 충청북도 괴산군 임꺽정로 113</span>
+            <b className="detail_b">주소</b>
+            <span className="detail_span"> 충청북도 괴산군 임꺽정로 113</span>
           </li>
         </ul>
-        {/* </Card.Body>
-        </Card> */}
       </div>
 
       {/* 지도 */}
@@ -57,7 +54,7 @@ const FestivalDetail = () => {
       <Map></Map>
       <br />
 
-      <div width="100%" height="400px" style={{ padding: '20px' }}>
+      <div className="detail_div">
         <strong>주변 맛집</strong>
         <hr style={{ height: '1px', background: 'black' }} />
         <ul class="list-group list-group-flush">
@@ -70,37 +67,39 @@ const FestivalDetail = () => {
   );
 };
 
+//주변 맛집 리스트 컴포넌트
 function AroundResCompo() {
   return (
-    <li class="list-group-item" style={{ display: 'inline' }}>
-      <img style={{ width: '50px', height: '50px', float: 'left' }} />
-      <span style={{ display: 'block' }}>
+    <li class="list-group-item inline">
+      <img className="list_img" />
+      <span className="block">
         <b style={{ marginLeft: '10px' }}>맛집 이름</b>
       </span>
-      <span style={{ display: 'block' }}>
+      <span className="block">
         <p style={{ fontSize: '12px', marginLeft: '60px' }}> 맛집 설명</p>{' '}
       </span>
     </li>
   );
 }
 
+//이미지 슬라이드 컴포넌트
 function DarkVariantExample() {
   return (
     <Carousel variant="dark">
       <Carousel.Item>
-        <img className="d-block w-100" alt="" style={{ width: '90%', height: '200px' }} />
+        <img className="d-block w-100 slide_img" alt="" />
         <Carousel.Caption>
           <h5>First slide label</h5>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" alt="" style={{ width: '90%', height: '200px' }} />
+        <img className="d-block w-100 slide_img" alt="" />
         <Carousel.Caption>
           <h5>Second slide label</h5>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" alt="" style={{ width: '90%', height: '200px' }} />
+        <img className="d-block w-100 slide_img" alt="" />
         <Carousel.Caption>
           <h5>Third slide label</h5>
         </Carousel.Caption>
