7a78c1c3e26a121677ce35d111d3f6c3bb26a045
노소연 <yeonie@kakao.com>
2022-08-17 17:12:53 +0900

    이미지 슬라이드

--- festajone/src/components/FestivalDetail/FestivalDetail.js
+++ festajone/src/components/FestivalDetail/FestivalDetail.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import Map from '../MapApi/Map';
 import './festivalDetail.css';
-import { Card } from 'react-bootstrap';
+import { Card, Carousel } from 'react-bootstrap';
 
 const FestivalDetail = () => {
   return (
@@ -15,30 +15,15 @@ const FestivalDetail = () => {
             <span>개최 지역 &nbsp;|&nbsp; 개최 일자</span>
           </td>
           <td>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="25"
-              height="25"
-              fill="currentColor"
-              class="bi bi-heart"
-              viewBox="0 0 16 16"
-              style={{ align: 'right' }}
-            >
-              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-            </svg>
+            <h1>
+              <i class="bi bi-heart"></i>
+            </h1>
           </td>
         </tr>
       </table>
 
       {/* 이미지가 이미지마다 크기가 다르면? */}
-      <div
-        width="100%"
-        height="100px"
-        align="center"
-        style={{ backgroundColor: '#eceeef', padding: '50px' }}
-      >
-        이미지
-      </div>
+      <DarkVariantExample></DarkVariantExample>
 
       <div width="100%" height="400px" style={{ padding: '20px' }}>
         <strong>상세정보</strong>
@@ -76,37 +61,52 @@ const FestivalDetail = () => {
         <strong>주변 맛집</strong>
         <hr style={{ height: '1px', background: 'black' }} />
         <ul class="list-group list-group-flush">
-          <li class="list-group-item" style={{ display: 'inline' }}>
-            <img style={{ width: '50px', height: '50px', float: 'left' }} />
-            <span style={{ display: 'block' }}>
-              <b style={{ marginLeft: '10px' }}>맛집 이름</b>
-            </span>
-            <span style={{ display: 'block' }}>
-              <p style={{ fontSize: '12px', marginLeft: '60px' }}> 맛집 설명</p>{' '}
-            </span>
-          </li>
-          <li class="list-group-item" style={{ display: 'inline' }}>
-            <img style={{ width: '50px', height: '50px', float: 'left' }} />
-            <span style={{ display: 'block' }}>
-              <b style={{ marginLeft: '10px' }}>맛집 이름</b>
-            </span>
-            <span style={{ display: 'block' }}>
-              <p style={{ fontSize: '12px', marginLeft: '60px' }}> 맛집 설명</p>{' '}
-            </span>
-          </li>
-          <li class="list-group-item" style={{ display: 'inline' }}>
-            <img style={{ width: '50px', height: '50px', float: 'left' }} />
-            <span style={{ display: 'block' }}>
-              <b style={{ marginLeft: '10px' }}>맛집 이름</b>
-            </span>
-            <span style={{ display: 'block' }}>
-              <p style={{ fontSize: '12px', marginLeft: '60px' }}> 맛집 설명</p>{' '}
-            </span>
-          </li>
+          <AroundResCompo></AroundResCompo>
+          <AroundResCompo></AroundResCompo>
+          <AroundResCompo></AroundResCompo>
         </ul>
       </div>
     </>
   );
 };
+
+function AroundResCompo() {
+  return (
+    <li class="list-group-item" style={{ display: 'inline' }}>
+      <img style={{ width: '50px', height: '50px', float: 'left' }} />
+      <span style={{ display: 'block' }}>
+        <b style={{ marginLeft: '10px' }}>맛집 이름</b>
+      </span>
+      <span style={{ display: 'block' }}>
+        <p style={{ fontSize: '12px', marginLeft: '60px' }}> 맛집 설명</p>{' '}
+      </span>
+    </li>
+  );
+}
+
+function DarkVariantExample() {
+  return (
+    <Carousel variant="dark">
+      <Carousel.Item>
+        <img className="d-block w-100" alt="" style={{ width: '90%', height: '200px' }} />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" alt="" style={{ width: '90%', height: '200px' }} />
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" alt="" style={{ width: '90%', height: '200px' }} />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
 
 export default FestivalDetail;
