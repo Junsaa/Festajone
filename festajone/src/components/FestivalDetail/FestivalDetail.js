26011195c27cfcf0d92839e59d23c4469f85d40b
노소연 <yeonie@kakao.com>
2022-08-17 16:09:36 +0900

    축제 상세 페이지 커밋

--- festajone/src/components/FestivalDetail/FestivalDetail.js
+++ festajone/src/components/FestivalDetail/FestivalDetail.js
@@ -1,11 +1,112 @@
-import React from 'react'
+/* global kakao */
+//npm i bootstrap-icons
+import React from 'react';
+import Map from '../MapApi/Map';
+import './festivalDetail.css';
+import { Card } from 'react-bootstrap';
 
 const FestivalDetail = () => {
   return (
-    <div>
-      FestivalDetail Page
-    </div>
-  )
-}
+    <>
+      <table width="100%" height="100px" align="center" style={{ padding: '30px' }}>
+        <tr>
+          <td style={{ textAlign: 'center' }}>
+            <h2>축제 이름</h2>
+            <span>개최 지역 &nbsp;|&nbsp; 개최 일자</span>
+          </td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              class="bi bi-heart"
+              viewBox="0 0 16 16"
+              style={{ align: 'right' }}
+            >
+              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+            </svg>
+          </td>
+        </tr>
+      </table>
 
-export default FestivalDetail
+      {/* 이미지가 이미지마다 크기가 다르면? */}
+      <div
+        width="100%"
+        height="100px"
+        align="center"
+        style={{ backgroundColor: '#eceeef', padding: '50px' }}
+      >
+        이미지
+      </div>
+
+      <div width="100%" height="400px" style={{ padding: '20px' }}>
+        <strong>상세정보</strong>
+        <hr style={{ height: '1px', background: 'black' }} />
+        {/* <Card>
+          <Card.Body> */}
+        <ul>
+          <li>
+            <b className="b">시작일</b>
+            <span className="span">2022.09.01</span>
+          </li>
+          <li>
+            <b className="b">종료일</b>
+            <span className="span">2022.09.01</span>
+          </li>
+          <li>
+            <b className="b">전화번호</b>
+            <span className="span"> 043-830-3463</span>
+          </li>
+          <li>
+            <b className="b">주소</b>
+            <span className="span"> 충청북도 괴산군 임꺽정로 113</span>
+          </li>
+        </ul>
+        {/* </Card.Body>
+        </Card> */}
+      </div>
+
+      {/* 지도 */}
+      <br />
+      <Map></Map>
+      <br />
+
+      <div width="100%" height="400px" style={{ padding: '20px' }}>
+        <strong>주변 맛집</strong>
+        <hr style={{ height: '1px', background: 'black' }} />
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item" style={{ display: 'inline' }}>
+            <img style={{ width: '50px', height: '50px', float: 'left' }} />
+            <span style={{ display: 'block' }}>
+              <b style={{ marginLeft: '10px' }}>맛집 이름</b>
+            </span>
+            <span style={{ display: 'block' }}>
+              <p style={{ fontSize: '12px', marginLeft: '60px' }}> 맛집 설명</p>{' '}
+            </span>
+          </li>
+          <li class="list-group-item" style={{ display: 'inline' }}>
+            <img style={{ width: '50px', height: '50px', float: 'left' }} />
+            <span style={{ display: 'block' }}>
+              <b style={{ marginLeft: '10px' }}>맛집 이름</b>
+            </span>
+            <span style={{ display: 'block' }}>
+              <p style={{ fontSize: '12px', marginLeft: '60px' }}> 맛집 설명</p>{' '}
+            </span>
+          </li>
+          <li class="list-group-item" style={{ display: 'inline' }}>
+            <img style={{ width: '50px', height: '50px', float: 'left' }} />
+            <span style={{ display: 'block' }}>
+              <b style={{ marginLeft: '10px' }}>맛집 이름</b>
+            </span>
+            <span style={{ display: 'block' }}>
+              <p style={{ fontSize: '12px', marginLeft: '60px' }}> 맛집 설명</p>{' '}
+            </span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default FestivalDetail;
---
+++ festajone/src/components/MapApi/Map.js
@@ -0,0 +1,48 @@
+/* global kakao */
+import React, { useEffect } from 'react';
+
+const { kakao } = window;
+
+const Map = () => {
+  useEffect(() => {
+    // 마커를 담을 배열입니다
+    var markers = [];
+
+    var container = document.getElementById('map');
+    var options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3
+    };
+
+    var map = new kakao.maps.Map(container, options);
+    //마커표시
+    var markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+    var marker = new kakao.maps.Marker({
+      position: markerPosition
+    });
+    marker.setMap(map);
+
+    // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+    var mapTypeControl = new kakao.maps.MapTypeControl();
+
+    // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+    // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+    //검색 후 목록 표시
+
+    // console.log("loading kakaomap");
+  }, []);
+
+  return (
+    <div align="center">
+      <div id="map" style={{ width: '95%', height: '200px' }}></div>
+    </div>
+  );
+};
+
+export default Map;
