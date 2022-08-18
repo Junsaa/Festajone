2ede67c186d4acb747683b1c559ddbf951ae2034
nodreamer <107825054+nodreamer@users.noreply.github.com>
2022-08-18 11:41:48 +0900

    커밋

---
+++ festajone/src/components/MainPage/MaingPageItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MaingPageItem = ({ data }) => {
+  const sampleFestival = {
+    title: '행사 이름',
+    content: '행사 내용',
+    firstimage2: '',
+    eventstartdate: '행사 시작 날짜',
+    eventenddate: '행사 종료 날짜'
+  };
+  return <div></div>;
+};
+
+export default MaingPageItem;
