3e356673191a0698d9d701a49bc1bbed8e2291ed
sarararararara <nasara1977@gmail.com>
2022-08-29 10:17:20 +0900

    게시판 css

--- festajone/src/components/BoardList/BoardDetail.js
+++ festajone/src/components/BoardList/BoardDetail.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+// import arrayIncludes from '../../../node_modules/lodash/_arrayIncludes';
+import './BoardDetail.css';
 
 const handleDelete = (e) => {
   console.log('handleDelete(board_num) =>', e.target.id);
@@ -21,11 +23,11 @@ const BoardDetail = ({ article, handlelist, handleupdateform }) => {
   var image1;
   var image2;
   var image3;
-  if(article.board_image2 === null && article.board_image3 === null){
+  if (article.board_image2 === null && article.board_image3 === null) {
     image1 = 'http://localhost:8008/uploads/' + article.board_image1;
     image2 = null;
     image3 = null;
-  } else if(article.board_image3 === null){
+  } else if (article.board_image3 === null) {
     image1 = 'http://localhost:8008/uploads/' + article.board_image1;
     image2 = 'http://localhost:8008/uploads/' + article.board_image2;
     image3 = null;
@@ -39,69 +41,75 @@ const BoardDetail = ({ article, handlelist, handleupdateform }) => {
     return (
       <div>
         <form>
-          <table border="1" width="700px" align="center">
-            <tr>
-              <td width="100px">글번호</td>
-              <td align="left" width="600px">
-                {article.board_num}
-              </td>
-            </tr>
-            <tr>
-              <td width="100px">제목</td>
-              <td align="left" width="600px">
-                {article.board_title}
-              </td>
-            </tr>
-            <tr>
-              <td width="100px">글쓴이</td>
-              <td align="left" width="600px">
-                {article.board_writer}
-              </td>
-            </tr>
-            <tr>
-              <td width="100px">글쓴날짜</td>
-              <td align="left" width="600px">
-                {article.board_date}
-              </td>
-            </tr>
-            <tr>
-              <td width="100px">글내용</td>
-              <td align="left" width="600px">
-                {article.board_content}
-              </td>
-            </tr>
-            <tr>
-              <td width="100px">이미지</td>
-              <td align="left" width="600px">
-                <img src={image1} />
-              </td> 
-              <td align="left" width="600px">
-                <img src={image2} />
-              </td>
-              <td align="left" width="600px">
-                <img src={image3} />
-              </td>
-            </tr>
-            <tr>
-              <td colspan="2" align="center">
-                <input type="button" value="글목록" onClick={handlelist}></input>
-                &nbsp;
-                <input
-                  type="button"
-                  value="수정"
-                  id={article.board_num}
-                  onClick={handleupdateform}
-                ></input>
-                &nbsp;
-                <input
-                  type="button"
-                  value="삭제"
-                  id={article.board_num}
-                  onClick={handleDelete}
-                ></input>
-              </td>
-            </tr>
-          </table>
+          <div className="board_wrap" align="center">
+            <div className="board_detail_wrap">
+              <div className="board_detail">
+                <div className="title">
+                  <dl align="left">
+                    <dt>{article.board_title}</dt>
+                    {/* <dt>{article.board_title}</dt> */}
+                  </dl>
+                </div>
+
+                <div className="info">
+                  <dl>
+                    <dt>글번호 : {article.board_num}</dt>
+                    {/* <dt>{article.board_num}</dt> */}
+                  </dl>
+
+                  <dl>
+                    <dt>글쓴이 : {article.board_writer}</dt>
+                    {/* <dt>{article.board_writer}</dt> */}
+                  </dl>
+
+                  <dl>
+                    <dt>글쓴날짜 : {article.board_date}</dt>
+                    {/* <dt>{article.board_date}</dt> */}
+                  </dl>
+                </div>
+
+                <div className="content">
+                  <dl>
+                    <dt>{article.board_content}</dt>
+                  </dl>
+                </div>
+
+                <div className="image">
+                  <dl>
+                    <dt>
+                      <img src={image1} alt="image1" />
+                    </dt>
+                    <dt>
+                      <img src={image2} alt="image2" />
+                    </dt>
+                    <dt>
+                      <img src={image3} alt="image3" />
+                    </dt>
+                  </dl>
+                </div>
+              </div>
+            </div>
+
+            <div className="select_btn">
+              <input className="list_but" type="button" value="목록" onClick={handlelist}></input>
+              &nbsp;
+              <input
+                className="update"
+                type="button"
+                value="수정"
+                id={article.board_num}
+                onClick={handleupdateform}
+              ></input>
+              &nbsp;
+              <input
+                className="delete"
+                type="button"
+                value="삭제"
+                id={article.board_num}
+                onClick={handleDelete}
+              ></input>
+            </div>
+          </div>
         </form>
       </div>
     );
@@ -109,58 +117,59 @@ const BoardDetail = ({ article, handlelist, handleupdateform }) => {
     return (
       <div>
         <form>
-          <table border="1" width="700px" align="center">
-            <tr>
-              <td width="100px">글번호</td>
-              <td align="left" width="600px">
-                {article.board_num}
-              </td>
-            </tr>
-            <tr>
-              <td width="100px">제목</td>
-              <td align="left" width="600px">
-                {article.board_title}
-              </td>
-            </tr>
-            <tr>
-              <td width="100px">글쓴이</td>
-              <td align="left" width="600px">
-                {article.board_writer}
-              </td>
-            </tr>
-            <tr>
-              <td width="100px">글쓴날짜</td>
-              <td align="left" width="600px">
-                {article.board_date}
-              </td>
-            </tr>
-            <tr>
-              <td width="100px">글내용</td>
-              <td align="left" width="600px">
-                {article.board_content}
-              </td>
-            </tr>
-            <tr>
-              <td width="100px">이미지</td>
-              <td align="left" width="600px">
-                <img src={image1} />
-              </td>
-              <td align="left" width="600px">
-                <img src={image2} />
-              </td>
-              <td align="left" width="600px">
-                <img src={image3} />
-              </td>
-            </tr>
-            <br/>
-            <br/>
-            <br/>
-            <tr>
-              <td colspan="2" align="center">
-                <input type="button" value="글목록" onClick={handlelist}></input>
-              </td>
-            </tr>
-          </table>
+          <div className="board_wrap" align="center">
+            <div className="board_detail_wrap">
+              <div className="board_detail">
+                <div className="title">
+                  <dl align="left">
+                    <dt>{article.board_title}</dt>
+                    {/* <dt>{article.board_title}</dt> */}
+                  </dl>
+                </div>
+
+                <div className="info">
+                  <dl>
+                    <dt>글번호 : {article.board_num}</dt>
+                    {/* <dt>{article.board_num}</dt> */}
+                  </dl>
+
+                  <dl>
+                    <dt>글쓴이 : {article.board_writer}</dt>
+                    {/* <dt>{article.board_writer}</dt> */}
+                  </dl>
+
+                  <dl>
+                    <dt>글쓴날짜 : {article.board_date}</dt>
+                    {/* <dt>{article.board_date}</dt> */}
+                  </dl>
+                </div>
+
+                <div className="content">
+                  <dl>
+                    <dt>{article.board_content}</dt>
+                  </dl>
+                </div>
+
+                <div className="image">
+                  <dl>
+                    <dt>
+                      <img src={image1} alt="image1" />
+                    </dt>
+                    <dt>
+                      <img src={image2} alt="image2" />
+                    </dt>
+                    <dt>
+                      <img src={image3} alt="image3" />
+                    </dt>
+                  </dl>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <input className="list_but" type="button" value="목록" onClick={handlelist}></input>
+            </div>
+          </div>
         </form>
       </div>
     );
--- festajone/src/components/BoardList/BoardUpdateForm.js
+++ festajone/src/components/BoardList/BoardUpdateForm.js
@@ -1,52 +1,74 @@
+import './BoardUpdateForm.css';
+
 const BoardUpdateForm = ({ article, setarticle, handleupdate, handlelist }) => {
-  console.log("BoardUpdateForm =>", article);
+  console.log('BoardUpdateForm =>', article);
 
   const onChange = (e) => {
     setarticle({
       ...article,
-      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value
     });
   };
+
   return (
     <div>
       <form>
-        <table border="1" width="700px" align="center">
-          <tr>
-            <td width="100px">제목</td>
-            <td align="left" width="600px">
-              <input
-                type="text"
-                name="board_title"
-                defaultValue={article.board_title}
-                onChange={onChange}
-              ></input>
-            </td>
-          </tr>
-          <tr>
-            <td width="100px">글쓴이</td>
-            <td align="left" width="600px">
-              {article.board_writer}
-            </td>
-          </tr>
-          <tr>
-            <td width="100px">글내용</td>
-            <td align="left" width="600px">
-              <input
-                type="text"
-                name="board_content"
-                defaultValue={article.board_content}
-                onChange={onChange}
-              ></input>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="2" align="center">
-              <input type="button" value="글수정" onClick={handleupdate}></input>
-              &nbsp;
-              <input type='button' value='리스트' onClick={handlelist}></input>
-            </td>
-          </tr>
-        </table>
+        <div className="board_wrap" align="center">
+          <div className="board_title" align="left">
+            글수정
+          </div>
+
+          <div className="board_update_wrap">
+            <div className="board_update">
+              <div className="title">
+                <dl>
+                  <dt className="update-title">제목</dt>
+                  <dt className="update-txt">
+                    <input
+                      type="text"
+                      name="board_title"
+                      defaultValue={article.board_title}
+                      onChange={onChange}
+                    ></input>
+                  </dt>
+                </dl>
+              </div>
+
+              <div className="writer">
+                <dl>
+                  <dt className="update-title">작성자</dt>
+                  <dt className="update-txt">{article.board_writer}</dt>
+                </dl>
+              </div>
+
+              <div className="content">
+                <dl>
+                  <dt className="update-title" border="1">
+                    내용
+                  </dt>
+                  <dt className="update-txt">
+                    <textarea
+                      type="text"
+                      name="board_content"
+                      defaultValue={article.board_content}
+                      onChange={onChange}
+                    ></textarea>
+                  </dt>
+                </dl>
+              </div>
+            </div>
+          </div>
+          <td colspan="2" align="center">
+            <input
+              type="button"
+              value="글수정"
+              className="update-btn"
+              onClick={handleupdate}
+            ></input>
+            &nbsp;
+            <input type="button" value="목록" className="list_but" onClick={handlelist}></input>
+          </td>
+        </div>
       </form>
     </div>
   );
